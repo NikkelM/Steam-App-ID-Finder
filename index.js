3771e01aab5758dfb519cbddf51c6f4c7b68124e
NikkelM <nikkel.mollenhauer@student.hpi.de>
2023-01-02 19:35:39 +0100

    Started modularization

--- index.js
+++ index.js
@@ -1,35 +1,79 @@
+// Author: NikkelM
+// Description: Matches game names to their Steam App ID's. Games whose names do not have an exact match in Steam's database are matched using string similarity.
+
 // Suppresses the warning about the fetch API being unstable
 process.removeAllListeners('warning');
 
+// Utility libraries
 import fs from 'fs';
+import jsonschema from 'jsonschema';
 import stringSimilarity from 'string-similarity';
+
+// Utility for getting the directory of the current file
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // ---------- Setup ----------
 
 // ----- Config -----
 
-// console.log("Loading config.json...");
-// import CONFIG from './config.json' assert { type: "json" };
-
-// ----- Other -----
-
-// Create the output directory if it doesn't exist
-if (!fs.existsSync('./output')) {
-	fs.mkdirSync('./output');
+try {
+	let configFileName;
+	if (fs.existsSync(__dirname + '/config.json')) {
+		console.log("Loading configuration file \"config.json\"...");
+		configFileName = 'config.json';
+	} else if (fs.existsSync(__dirname + '/config.default.json')) {
+		console.log("!!! No custom configuration file found! Loading default configuration file \"config.default.json\"...");
+		configFileName = 'config.default.json';
+	}
+	var CONFIG = JSON.parse(fs.readFileSync(__dirname + '/' + configFileName));
+} catch (error) {
+	console.error("Error loading configuration file: " + error);
+	process.exit(1);
 }
 
-// A JSON object with all Steam games
-console.log("Fetching Steam games...");
-const steamApps = await fetch("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
-	.then((response) => response.json())
-	.then((data) => data.applist.apps);
+// Validate the config file against the schema
+console.log("Validating configuration file...\n");
+try {
+	const validator = new jsonschema.Validator();
+	validator.validate(CONFIG, JSON.parse(fs.readFileSync(__dirname + '/config.schema.json')), { throwError: true });
+} catch (error) {
+	console.error("Error validating configuration file: " + error);
+	process.exit(1);
+}
 
-console.log("Found " + steamApps.length + " Steam games.\n");
+// ----- Output -----
+
+// Create the output directory if it doesn't exist
+if (!fs.existsSync(__dirname + './output')) {
+	fs.mkdirSync(__dirname + './output');
+}
+
+// ---------- Main ----------
+
+main();
+
+async function main() {
+	// Fetch Steam games from API
+	const steamApps = await fetchSteamApps();
+}
+
+async function fetchSteamApps() {
+	// A JSON object with all Steam games
+	console.log("Fetching Steam games...");
+	const steamApps = await fetch("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
+		.then((response) => response.json())
+		.then((data) => data.applist.apps);
+
+	console.log("Found " + steamApps.length + " Steam games.\n");
+}
 
 // ---------- Import the game names ----------
 
 let gameNames = fs.readFileSync("./input/gameNames.txt", "utf-8");
-gameNames = Object.assign({}, ...gameNames.split("\r\n").map((game) => ({[game]: -1})));
+gameNames = Object.assign({}, ...gameNames.split("\r\n").map((game) => ({ [game]: -1 })));
 
 const numGameNames = Object.keys(gameNames).length;
 console.log("\"./input/gameNames.txt\" contained " + numGameNames + " game names for which to find the Steam App ID's.\n");
@@ -61,7 +105,7 @@ fs.writeFileSync('./output/steamAppIds_fullMatch.json', JSON.stringify(steamIDsF
 
 console.log("Searching for best matches using string similarity...\n");
 
-for(const app in steamApps) {
+for (const app in steamApps) {
 	steamApps[app].name = steamApps[app].name.toLowerCase();
 }
 
